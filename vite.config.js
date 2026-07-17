c5775ab3f80526bcdd7e42391d7f32527ccb4ac5
ASUS <suhankarkera160@gmail.com>
2026-07-17 13:19:20 +0530

    Update vite config and enable debug build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  build: {
+    // This forces Vite to be more robust during the build process
+    target: 'esnext',
+    minify: 'esbuild',
+    chunkSizeWarningLimit: 1000,
+  }
+})
